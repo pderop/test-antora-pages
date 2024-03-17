a5037eb296c528c0890151e45f871684a0ebd4fd
Pierre De Rop <pierre.de-rop@broadcom.com>
2024-03-18 00:02:16 +0100

    test

--- .github/scripts/auto-index.sh
+++ .github/scripts/auto-index.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 generate_index() {
-    local directory=$1
-    local index_path="${directory}/index.html"
-    {
-        echo "<html><head><title>Index of \"${directory}\"</title></head><body>"
-        echo "<h1>Index of \"${directory}\":</h1>"
-        echo "<ul>"
-        for item in $(ls -dtr "${directory}"/*); do
-            if [ -d "${item}" ]; then
-		local item_name=$(basename "${item}")
-                echo "<li><a href=\"${item_name}/\">${item_name}</a></li>"
-            fi
-        done
-        echo "</ul></body></html>"
-    } > "${index_path}"
+  local directory=$1
+  local index_path="${directory}/index.html"
+  {
+    echo "<html><head><title>Index of \"${directory}\"</title></head><body>"
+    echo "<h1>Index of \"${directory}\":</h1>"
+    echo "<ul>"
+    for item in $(ls -dtr "${directory}"/*); do
+      if [ -d "${item}" ]; then
+        local item_name=$(basename "${item}")
+        echo "<li><a href=\"${item_name}/\">${item_name}</a></li>"
+      fi
+    done
+    echo "</ul></body></html>"
+  } >"${index_path}"
 }
 
 generate_html() {
-    local directory=$1
-    generate_index "${directory}"
-    for item in "${directory}"/*; do
-        if [ -d "${item}" ] && [ ! -L "${item}" ]; then
-	    local item_name=$(basename "${item}")
-            if [ "${item_name}" = "reference" ] || [ "${item_name}" = "javadoc-api" ]; then
-                continue  # Stop recursion for "reference" or "javadoc-api" directories
-            fi
-            generate_html "${item}"
-        fi
-    done
+  local directory=$1
+  generate_index "${directory}"
+  for item in "${directory}"/*; do
+    if [ -d "${item}" ] && [ ! -L "${item}" ]; then
+      local item_name=$(basename "${item}")
+      if [ "${item_name}" = "reference" ] || [ "${item_name}" = "javadoc-api" ]; then
+        continue # Stop recursion for "reference" or "javadoc-api" directories
+      fi
+      generate_html "${item}"
+    fi
+  done
 }
 
 # Root directory
